12ed0f3aa9c872eb483e3fe7cbc9c7b8c6940b0b
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2022-01-31 15:08:35 -0600

    making pages

--- accesskey.js
+++ accesskey.js
@@ -1,5 +1,4 @@
 // accessKey   Sets or returns the accesskey attribute of an element
 
-document.getElementById('example').accesskey= 'example';
-
-console.log('example');
+document.getElementById('example').accessKey = 'example';
+console.log('example')
